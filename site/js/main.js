16446b5aa9c41dfbfc5640eec8e277d8c156872b
henryfeinstein <98499896+henryfeinstein@users.noreply.github.com>
2023-03-13 14:34:30 -0400

    setting up web app files

---
+++ site/js/main.js
@@ -0,0 +1,6 @@
+import { initializeMap } from "./map.js";
+
+// setup
+
+// initialize map
+let map = initializeMap();
---
+++ site/js/map.js
@@ -0,0 +1,13 @@
+function initializeMap() {
+    let map = L.map('map').setView([38.9092301970931, -77.03380390100355], 8);
+
+    const mapboxAccount = 'mapbox';
+    const mapboxStyle = 'light-v10';
+    const mapboxToken = 'pk.eyJ1IjoiaGVucnlmZWluc3RlaW4iLCJhIjoiY2w4dzIyYXc0MDN2dTNwcnE3ZnMzOXh5OCJ9.Xj0CS62yWWvKB-v_uYz9sQ';
+    L.tileLayer(`https://api.mapbox.com/styles/v1/${mapboxAccount}/${mapboxStyle}/tiles/256/{z}/{x}/{y}@2x?access_token=${mapboxToken}`, {
+    maxZoom: 19,
+    attribution: '© <a href="https://www.mapbox.com/about/maps/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> <strong><a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a></strong>',
+    }).addTo(map);
+
+    return map;
+}
