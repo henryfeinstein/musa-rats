8d33162388f2842d002db813a18a3e8fb9cc277f
henryfeinstein <98499896+henryfeinstein@users.noreply.github.com>
2023-03-13 16:03:44 -0400

    continued site setup

--- site/js/main.js
+++ site/js/main.js
@@ -1,6 +1,47 @@
 import { initializeMap } from "./map.js";
 
-// setup
-
 // initialize map
 let map = initializeMap();
+
+// get DOMs
+let listButton = document.querySelector("#list-button");
+let mapButton = document.querySelector("#map-button");
+let analysisButton = document.querySelector("#analysis-button");
+let listContainer = document.querySelector("#list-container");
+
+// event listener functions
+function onListButtonClick() {
+    listButton.classList.remove("unpressed");
+    listButton.classList.add("pressed");
+    mapButton.classList.remove("pressed");
+    mapButton.classList.add("unpressed");
+    analysisButton.classList.remove("pressed");
+    analysisButton.classList.add("unpressed");
+    listContainer.classList.add("list-up");
+    listContainer.classList.remove("list-down");
+}
+
+function onMapButtonClick() {
+    listButton.classList.remove("pressed");
+    listButton.classList.add("unpressed");
+    mapButton.classList.remove("unpressed");
+    mapButton.classList.add("pressed");
+    analysisButton.classList.remove("pressed");
+    analysisButton.classList.add("unpressed");
+    listContainer.classList.remove("list-up");
+    listContainer.classList.add("list-down");
+}
+
+function onAnalysisButtonClick() {
+    listButton.classList.remove("pressed");
+    listButton.classList.add("uppressed");
+    mapButton.classList.remove("pressed");
+    mapButton.classList.add("unpressed");
+    analysisButton.classList.remove("unpressed");
+    analysisButton.classList.add("pressed");
+}
+
+// event listeners
+listButton.addEventListener('click', onListButtonClick);
+mapButton.addEventListener('click', onMapButtonClick);
+analysisButton.addEventListener('click', onAnalysisButtonClick);
