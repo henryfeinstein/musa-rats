42243cb72e66801cb7574a0ad4bc93650cc2bde0
Yesao <673392438@qq.com>
2023-04-12 13:47:57 -0400

    Add search bar, and click function

--- site/js/map.js
+++ site/js/map.js
@@ -11,22 +11,36 @@ function initializeMap() {
         zoom: 9, // starting zoom
     });
 
+    // Add the control to the map.
+    const geocoder = new MapboxGeocoder({
+        accessToken: mapboxgl.accessToken,
+        mapboxgl: mapboxgl,
+        language: 'en-EN',
+    });
+
+    document.getElementById('geocoder').appendChild(geocoder.onAdd(map));
+
+    // Add geolocate control to the map.
+    map.addControl(
+        new mapboxgl.GeolocateControl({
+            positionOptions: {
+                enableHighAccuracy: true,
+            },
+            // When active the map will receive updates to the device's location as it changes.
+            trackUserLocation: true,
+            // Draw an arrow next to the location dot to indicate which direction the device is heading.
+            showUserHeading: true,
+        }),
+    );
+
+    map.addControl(new mapboxgl.NavigationControl());
+
     return map;
 }
 
 async function initializeBlocks(map) {
     // pull city block shapefile
     let blocks = await fetchJSON('./data/city_blocks.geojson');
-
-    // add to new map layer
-    // map.blockLayer = L.geoJSON(blocks, {
-    //     style: {
-    //         opacity: 0.3,
-    //         fillOpacity: 0,
-    //         weight: 1,
-    //         color: "#000000",
-    //     },
-    // }).addTo(map);
 
     map.on('load', () => {
         // Add a data source containing GeoJSON data.
@@ -59,6 +73,73 @@ async function initializeBlocks(map) {
                 'line-width': 1,
             },
         });
+    });
+
+    // When clicked, show the block information
+    map.on('click', 'blocks', (e) => {
+        console.log(e)
+        let features = map.queryRenderedFeatures(e.point, { layers: ['blocks'] });
+        console.log(features);
+        let clicked_block = features[0].toJSON()['properties']['block_id'];
+
+        // $("#geocoder-roof").trigger('change');
+
+        // clicked_address = "";
+
+        const coordinates = features[0].toJSON()["geometry"]["coordinates"][0];
+
+        // Create a 'LngLatBounds' with both corners at the first coordinate.
+        const bounds = new mapboxgl.LngLatBounds(
+            coordinates[0],
+            coordinates[0]
+        );
+
+        // Extend the 'LngLatBounds' to include every coordinate in the bounds result.
+        for (const coord of coordinates) {
+            bounds.extend(coord);
+        }
+
+        map.fitBounds(bounds, {
+            padding: 20
+        });
+
+        let feature = features[0].toJSON();
+
+        if (typeof map.getLayer('selectedBlock') !== "undefined" ){
+            map.removeLayer('selectedBlock');
+            map.removeSource('selectedBlock');
+        }
+
+        map.addSource('selectedBlock', {
+            "type":"geojson",
+            "data": feature,
+        });
+        map.addLayer({
+            "id": "selectedBlock",
+            "type": "line",
+            "source": "selectedBlock",
+            "layout": {
+                "line-join": "round",
+                "line-cap": "round",
+            },
+            "paint": {
+                "line-color": "white",
+                "line-width": 6,
+            },
+        });
+        //
+        new mapboxgl.Popup()
+            .setLngLat(e.lngLat)
+            .setHTML(e.features[0].properties.block_id)
+            .addTo(map);
+    });
+
+    map.on('mouseenter', 'blocks', () => {
+        map.getCanvas().style.cursor = 'pointer';
+    });
+
+    map.on('mouseleave', 'blocks', () => {
+        map.getCanvas().style.cursor = '';
     });
 
     return blocks;
