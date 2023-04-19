ab40a070088ccc147623b1355c8a5c2b16f916d0
Yesao <673392438@qq.com>
2023-04-19 16:51:52 -0400

    Add search bar, and click function

--- site/js/main.js
+++ site/js/main.js
@@ -1,4 +1,4 @@
-import { initializeBlocks, initializeMap, addHotspot } from "./map.js";
+import { initializeBlocks, initializeMap } from "./map.js";
 import { parseRequestData, buildPredictionList } from "./dataProcessing.js";
 
 // initialize site
@@ -11,34 +11,13 @@ buildPredictionList(requests);
 window.blocks = blocks;
 window.requests = requests;
 
-// event handlers
-function onRatHotspotCheck() {
-    if (ratHotspotCheckbox.checked) {
-        callHotspotCheckbox.checked = false;
-        let path = 'rat-hotspot';
-        addHotspot(map, path);
-    }
-}
-
-function onCallHotspotCheck() {
-    if (callHotspotCheckbox.checked) {
-        ratHotspotCheckbox.checked = false;
-        let path = 'call-hotspot';
-        addHotspot(map, path);
-    }
-}
-
-function onWardMenuSelection() {
-    console.log(wardMenu.value);
-}
-
 // get DOMs
 let listContainer = document.querySelector("#list-container");
-let wardMenu = document.querySelector("#ward-menu");
-let ratHotspotCheckbox = document.querySelector("#rat-hotspot");
-let callHotspotCheckbox = document.querySelector("#call-hotspot");
-
-// event listeners
-ratHotspotCheckbox.addEventListener('change', onRatHotspotCheck);
-callHotspotCheckbox.addEventListener('change', onCallHotspotCheck);
-wardMenu.addEventListener('change', onWardMenuSelection);
+// let wardMenu = document.querySelector("#ward-menu");
+// let ratHotspotCheckbox = document.querySelector("#rat-hotspot");
+// let callHotspotCheckbox = document.querySelector("#call-hotspot");
+//
+// // event listeners
+// ratHotspotCheckbox.addEventListener('change', onRatHotspotCheck);
+// callHotspotCheckbox.addEventListener('change', onCallHotspotCheck);
+// wardMenu.addEventListener('change', onWardMenuSelection);
--- site/js/map.js
+++ site/js/map.js
@@ -8,7 +8,7 @@ async function initializeMap() {
         // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
         style: 'mapbox://styles/mapbox/light-v9', // style URL
         center: [-77.03380390100355, 38.9092301970931], // starting position [lng, lat],
-        zoom: 9, // starting zoom
+        zoom: 11, // starting zoom
     });
 
     // Add the control to the map.
@@ -38,15 +38,30 @@ async function initializeMap() {
     // load data for hotspots
     let ratHotspot = await fetchJSON('./data/rat_hotspot.geojson');
     let callHotspot = await fetchJSON('./data/req_hotspot.geojson');
+    let wards = await fetchJSON('./data/Wards.geojson');
 
-    map.addSource('rat-hotspot', {
+    // store my layers, for the convenience of removing
+    let myLayers = [];
+
+    window.wards = wards;
+    window.map = map;
+    window.myLayers = myLayers;
+
+    map.addSource('rats-hotspot', {
         'type': 'geojson',
         'data': ratHotspot
     });
 
+    console.log(ratHotspot)
+
     map.addSource('call-hotspot', {
         'type': 'geojson',
         'data': callHotspot
+    });
+
+    map.addSource('wards', {
+        'type': 'geojson',
+        'data': wards
     });
 
     return map;
@@ -77,6 +92,9 @@ async function initializeBlocks(map) {
                 'fill-opacity': 0.1,
             },
         });
+
+        myLayers.push('blocks');
+
         map.addLayer({
             'id': 'outline',
             'type': 'line',
@@ -87,6 +105,39 @@ async function initializeBlocks(map) {
                 'line-width': 1,
             },
         });
+
+        myLayers.push('outline');
+
+        map.addLayer({
+            'id': 'wards',
+            'type': 'fill',
+            'source': 'wards', // reference the data source
+            'layout': {},
+            'generateId': true,
+            'paint': {
+                // 'fill-color': '#0080ff', // blue color fill
+                // 'fill-opacity': 0,
+                'fill-outline-color': 'rgba(0,0,0,0.1)',
+                // 'fill-color': '#627BC1',
+                'fill-opacity': 0,
+            },
+        });
+
+        myLayers.push('wards');
+
+        map.addLayer({
+            'id': 'ward_outline',
+            'type': 'line',
+            'source': 'wards',
+            'layout': {},
+            'paint': {
+                'line-color': '#fa0808',
+                'line-width': 1,
+            },
+        });
+
+        myLayers.push('ward_outline');
+
     });
 
     // When clicked, show the block information
@@ -107,6 +158,8 @@ async function initializeBlocks(map) {
             coordinates[0],
             coordinates[0]
         );
+
+        console.log(bounds)
 
         // Extend the 'LngLatBounds' to include every coordinate in the bounds result.
         for (const coord of coordinates) {
@@ -141,7 +194,9 @@ async function initializeBlocks(map) {
                 "line-width": 6,
             },
         });
-        //
+
+        myLayers.push('selectedBlock');
+
         new mapboxgl.Popup()
             .setLngLat(e.lngLat)
             .setHTML(e.features[0].properties.block_id)
@@ -159,31 +214,158 @@ async function initializeBlocks(map) {
     return blocks;
 }
 
-function addHotspot(map, type) {
-    //if hotspot layer is already present, remove it
-    if (map.getLayer('hotspot')) {
-        map.removeLayer('hotspot');
+$("#select-ward").change(function(){
+    let selectVal = $("#select-ward option:selected").val();
+
+    for (let i = 0; i < wards['features'].length; i++) {
+        if (wards['features'][i]['properties']['NAME'] === selectVal) {
+            const coordinates = wards.features[i].geometry.coordinates[0];
+
+            // Create a 'LngLatBounds' with both corners at the first coordinate.
+            const bounds = new mapboxgl.LngLatBounds(
+                coordinates[0],
+                coordinates[0],
+            );
+
+            // Extend the 'LngLatBounds' to include every coordinate in the bounds result.
+            for (const coord of coordinates) {
+                bounds.extend(coord);
+            }
+
+            map.fitBounds(bounds, {
+                padding: 20,
+            });
+
+            let feature = wards['features'][i];
+
+            if (typeof map.getLayer('selectedWard') !== "undefined" ){
+                map.removeLayer('selectedWard');
+                map.removeSource('selectedWard');
+            }
+
+            map.addSource('selectedWard', {
+                "type":"geojson",
+                "data": feature,
+            });
+            map.addLayer({
+                "id": "selectedWard",
+                "type": "line",
+                "source": "selectedWard",
+                "layout": {
+                    "line-join": "round",
+                    "line-cap": "round",
+                },
+                "paint": {
+                    "line-color": "red",
+                    "line-width": 6,
+                },
+            });
+
+            myLayers.push('selectedWard');
+
+            break;
+        }
     }
+});
+
+// Clear all layers except for the basic layers: ward_outline, wards, blocks-boundary, blocks
+
+function clearLayers(map) {
+    let basic_layers = ['blocks', 'outline', 'wards', 'ward_outline'];
+    for (let i = 0; i < myLayers.length; i++) {
+        if (basic_layers.indexOf(myLayers[i]) === -1) {
+            map.removeLayer(myLayers[i]);
+
+            // delete the layerId in myLayers
+            let index = myLayers.indexOf(3); // find the index of the element to be removed
+            if (index !== -1) {
+              myLayers.splice(index, 1); // remove the element at the specified index
+            }
+        }
+    }
+}
+
+$("#rats-hotspots").click(function(){
+
+    clearLayers(map);
+
+    // map.addLayer({
+    //   'id': 'call-hotspot',
+    //   'type': 'fill',
+    //   'source': 'rats-hotspot',
+    //   'layout': {},
+    //   'paint': {
+    //     'fill-color': [
+    //       'interpolate',
+    //       ['linear'],
+    //       ['get', 'Rat_Count'], // 用于渐变的属性
+    //         7, 'rgb(235,59,31)',
+    //         20, 'rgb(255,129,43)',
+    //         36, 'rgb(243,175,36)',
+    //         56, 'rgb(149,210,105)',
+    //         83, 'rgb(84,159,40)',
+    //         119, 'rgb(81,120,254)',
+    //         165, 'rgb(33,86,254)',
+    //         235, 'rgb(56,3,219)',
+    //         452, 'rgb(31,3,106)' // 从0到100的范围内渐变颜色
+    //     ],
+    //     'fill-opacity': 0.7
+    //   }
+    // });
 
     map.addLayer({
-        'id': 'hotspot',
+        'id': 'rats-hotspot',
         'type': 'fill',
-        'source': type, // reference the data source
+        'source': 'rats-hotspot', // reference the data source
         'layout': {},
-        'generateId': true,
         'paint': {
-            'fill-color': '#0080ff', // blue color fill
-            'fill-opacity': 0,
-            'fill-outline-color': 'rgba(0,0,0,0.1)',
-            'fill-color': '#627BC1',
-            'fill-opacity': 0.1,
-        },
+            'fill-color': [
+              'interpolate',
+              ['linear'],
+              ['get', 'Rat_Count'],
+                2, 'rgb(235,59,31)',
+                7, 'rgb(243,175,36)',
+                16, 'rgb(84,159,40)',
+                35, 'rgb(33,86,254)',
+                52, 'rgb(31,3,106)'
+            ],
+            'fill-opacity': 0.7
+        }
     });
+    myLayers.push('rats-hotspot');
+});
 
-}
+$("#311-hotspots").click(function(){
+
+    clearLayers(map);
+
+    map.addLayer({
+      'id': 'call-hotspot',
+      'type': 'fill',
+      'source': 'call-hotspot',
+      'layout': {},
+      'paint': {
+        'fill-color': [
+          'interpolate',
+          ['linear'],
+          ['get', 'Req_Count'],
+            7, 'rgb(235,59,31)', // value range & color
+            20, 'rgb(255,129,43)',
+            36, 'rgb(243,175,36)',
+            56, 'rgb(149,210,105)',
+            83, 'rgb(84,159,40)',
+            119, 'rgb(81,120,254)',
+            165, 'rgb(33,86,254)',
+            235, 'rgb(56,3,219)',
+            452, 'rgb(31,3,106)'
+        ],
+        'fill-opacity': 0.7
+      }
+    });
+    myLayers.push('call-hotspot');
+});
 
 export {
     initializeMap,
     initializeBlocks,
-    addHotspot,
 }
